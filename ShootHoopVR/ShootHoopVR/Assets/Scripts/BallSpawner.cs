1a32c2c2e7da6c5ac598650b10a6e569c5412c84
TimTom <tim.thomas11@gmail.com>
2016-12-28 21:25:20 -0700

    Instanstiate ball

---
+++ ShootHoopVR/ShootHoopVR/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 randomVector = new Vector3
+            (ballPrefab.transform.position.x + Random.Range(-1f,1f),
+            ballPrefab.transform.position.y,
+            ballPrefab.transform.position.z + Random.Range(-1f, 1f));
+
+        if (Input.GetKeyDown(KeyCode.Space))Instantiate(ballPrefab, randomVector,Quaternion.identity);
+	}
+}
